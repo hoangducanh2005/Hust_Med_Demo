1943c2a92ad3cc09aba1f351dafe65b07eb391d5
hoangducanh2005 <anh.hd235640@sis.hust.edu.vn>
2026-04-22 17:43:48 +0700

    add more songs

--- app/src/main/java/com/example/hust_med_demo/AutomotiveLibraryCallback.kt
+++ app/src/main/java/com/example/hust_med_demo/AutomotiveLibraryCallback.kt
@@ -25,6 +25,12 @@ class AutomotiveLibraryCallback : MediaLibraryService.MediaLibrarySession.Callba
     private val uri3 = RawResourceDataSource.buildRawResourceUri(R.raw.con_mua_tinh_yeu)
     private val uri4 = RawResourceDataSource.buildRawResourceUri(R.raw.mot_minh_mot_som_ban_mai)
     private val uri5 = RawResourceDataSource.buildRawResourceUri(R.raw.thang_tu_la_loi_noi_doi_cua_em)
+    private val uri6 = RawResourceDataSource.buildRawResourceUri(R.raw.baby)
+    private val uri7 = RawResourceDataSource.buildRawResourceUri(R.raw.havana)
+    private val uri8 = RawResourceDataSource.buildRawResourceUri(R.raw.haruharu)
+    private val uri9 = RawResourceDataSource.buildRawResourceUri(R.raw.shapeofyou)
+    private val uri10 = RawResourceDataSource.buildRawResourceUri(R.raw.loveyourself)
+    private val uri11 = RawResourceDataSource.buildRawResourceUri(R.raw.aslongasyouloveme)
 
     // Khởi tạo đường dẫn URI tới ảnh bìa trong res/drawable
     private val art1 = Uri.parse("android.resource://$pkg/drawable/art_kiep_ve_sau")
@@ -32,6 +38,12 @@ class AutomotiveLibraryCallback : MediaLibraryService.MediaLibrarySession.Callba
     private val art3 = Uri.parse("android.resource://$pkg/drawable/art_con_mua_tinh_yeu")
     private val art4 = Uri.parse("android.resource://$pkg/drawable/art_mot_minh_mot_som_ban_mai")
     private val art5 = Uri.parse("android.resource://$pkg/drawable/art_thang_tu_la_loi_noi_doi_cua_em")
+    private val art6 = Uri.parse("android.resource://$pkg/drawable/baby")
+    private val art7 = Uri.parse("android.resource://$pkg/drawable/havana")
+    private val art8 = Uri.parse("android.resource://$pkg/drawable/haruharu")
+    private val art9 = Uri.parse("android.resource://$pkg/drawable/shapeofyou")
+    private val art10 = Uri.parse("android.resource://$pkg/drawable/loveyourself")
+    private val art11 = Uri.parse("android.resource://$pkg/drawable/aslongasyouloveme")
 
     // Map mediaId -> URI file nhạc thật
     private val tracks: Map<String, Uri> = mapOf(
@@ -39,7 +51,13 @@ class AutomotiveLibraryCallback : MediaLibraryService.MediaLibrarySession.Callba
         "track_2" to uri2,
         "track_3" to uri3,
         "track_4" to uri4,
-        "track_5" to uri5
+        "track_5" to uri5,
+        "track_6" to uri6,
+        "track_7" to uri7,
+        "track_8" to uri8,
+        "track_9" to uri9,
+        "track_10" to uri10,
+        "track_11" to uri11
     )
 
     // Map mediaId -> URI ảnh bìa
@@ -48,7 +66,13 @@ class AutomotiveLibraryCallback : MediaLibraryService.MediaLibrarySession.Callba
         "track_2" to art2,
         "track_3" to art3,
         "track_4" to art4,
-        "track_5" to art5
+        "track_5" to art5,
+        "track_6" to art6,
+        "track_7" to art7,
+        "track_8" to art8,
+        "track_9" to art9,
+        "track_10" to art10,
+        "track_11" to art11
     )
 
     // Hàm helper để tạo MediaItem
@@ -74,7 +98,13 @@ class AutomotiveLibraryCallback : MediaLibraryService.MediaLibrarySession.Callba
         createMediaItem("track_2", "Hoa Hồng", "Hà Anh Tuấn", uri2, art2),
         createMediaItem("track_3", "Cơn Mưa Tình Yêu", "Hà Anh Tuấn", uri3, art3),
         createMediaItem("track_4", "Một mình một sớm ban mai", "Hà Anh Tuấn", uri4, art4),
-        createMediaItem("track_5", "Tháng tư là lời nói dối của em", "Hà Anh Tuấn", uri5, art5)
+        createMediaItem("track_5", "Tháng tư là lời nói dối của em", "Hà Anh Tuấn", uri5, art5),
+        createMediaItem("track_6", "Baby", "Justin Bieber", uri6, art6),
+        createMediaItem("track_7", "Havana", "Camila Cabello", uri7, art7),
+        createMediaItem("track_8", "Haru Haru", "BIGBANG", uri8, art8),
+        createMediaItem("track_9", "Shape of You", "Ed Sheeran", uri9, art9),
+        createMediaItem("track_10", "Love Yourself", "Justin Bieber", uri10, art10),
+        createMediaItem("track_11", "As Long As You Love Me", "Justin Bieber", uri11, art11)
     )
 
     override fun onGetLibraryRoot(
